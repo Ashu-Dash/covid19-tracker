da50ba448367c85b6372d05a1fbfe1597ca751f1
Ashutosh Dash <ashudash1991@gmail.com>
2020-03-20 02:08:17 +0530

    Added required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { CountriesComponent } from './countries/countries.component';
+import { CountryListComponent } from './countries/country-list/country-list.component';
+import { CountryDetailsComponent } from './countries/country-details/country-details.component';
+import { CountryItemComponent } from './countries/country-list/country-item/country-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountriesComponent,
+    CountryListComponent,
+    CountryDetailsComponent,
+    CountryItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
